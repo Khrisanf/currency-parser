f9b69b81ae5122d258ad1fa53bc6a300277a4bba
Khrisanf <161630808+Khrisanf@users.noreply.github.com>
2025-10-30 18:41:15 +0300

    Revert "Concurrency"

--- src/main/java/ru/netology/currencyparser/repository/CurrencyRateRepository.java
+++ src/main/java/ru/netology/currencyparser/repository/CurrencyRateRepository.java
@@ -1,8 +1,5 @@
 package ru.netology.currencyparser.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.netology.currencyparser.domain.CurrencyRate;
 
 import java.time.LocalDate;
@@ -10,15 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CurrencyRateRepository extends JpaRepository<CurrencyRate, Long> {
-
     Optional<CurrencyRate> findTopByCodeOrderByAsOfDateDesc(String code);
-
     List<CurrencyRate> findByAsOfDate(LocalDate date);
-
-    boolean existsByCodeAndAsOfDate(String code, LocalDate asOfDate);
-
-    Optional<CurrencyRate> findTopByCodeAndAsOfDateLessThanOrderByAsOfDateDesc(String code, LocalDate asOfDate);
-
-    @Query("select max(c.asOfDate) from CurrencyRate c where c.asOfDate <= :date")
-    LocalDate findMaxAsOfDateLe(@Param("date") LocalDate date);
+    Optional<CurrencyRate> findByCodeAndAsOfDate(String code, LocalDate date);
 }
